228f802991715a5951dbed51a02bde72d358b6ae
Luka Mania <manialuka19@gmail.com>
2025-08-12 03:16:14 +0400

    chore(test): extract `tmp_path` fixture into `conftest.py`

--- tests/test_text_file_writer.py
+++ tests/test_text_file_writer.py
@@ -1,16 +1,5 @@
 import pytest
-import tempfile
-import os
 from file_handlers.base_file_writers import TextFileWriter
-
-# Create a temporary file and provide path to it
-@pytest.fixture
-def tmp_path():
-    with tempfile.NamedTemporaryFile('w+', delete=False, encoding='utf-8') as tmp:
-        tmp_path = tmp.name
-
-    yield tmp_path
-    os.remove(tmp.name)
 
 @pytest.mark.parametrize(
     'content',
