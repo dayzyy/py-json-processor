bfdc61b12a2f41f4d6cbf28f30a9e24ac9292a63
Luka Mania <manialuka19@gmail.com>
2025-08-12 00:45:12 +0400

    chore: implement `TextFileWriter` class
    - Add a writer class that supprots writing to any text formated file
      (JSON, XML, CSV etc.)

--- file_handlers/base_file_writes.py
+++ file_handlers/base_file_writes.py
@@ -5,3 +5,10 @@ class BaseFileWrite(ABC):
     @abstractmethod
     def write(cls, file_path: str, data: str) -> None:
         raise NotImplementedError
+
+# Supports writing to any text formatted file (JSON, XML, CSV etc.)
+class TextFileWriter(BaseFileWrite):
+    @classmethod
+    def write(cls, file_path: str, data: str) -> None:
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(data)
