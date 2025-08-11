45b408fa2ee0cbe00fa237dd1e195d2fdd55f83b
Luka Mania <manialuka19@gmail.com>
2025-08-12 00:39:05 +0400

    chore: implement `TextFileReader` class
    - Add a reader class that handles reading from any text formatted file
      (JSON, XML, CSV etc.)

--- file_handlers/base_file_readers.py
+++ file_handlers/base_file_readers.py
@@ -5,3 +5,10 @@ class BaseFileReader(ABC):
     @abstractmethod
     def read(cls, file_path: str):
         raise NotImplementedError
+
+# Supports reading from any text formatted file (JSON, XML, CSV etc.)
+class TextFileReader(BaseFileReader):
+    @classmethod
+    def read(cls, file_path: str) -> str:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            return f.read()
