fba5815b2d526e4dd1fe8e5945c152561748426f
Luka Mania <manialuka19@gmail.com>
2025-08-11 18:00:07 +0400

    test: add validation test for missing attribute in `Model.__init__

--- tests/test_model.py
+++ tests/test_model.py
@@ -8,5 +8,13 @@ def test_model_init_raises_for_unexpected_attribute():
         name: str
         room: int
 
-    with pytest.raises(ValueError, match="Unexpected attribute 'last_name' while instantiating class 'Student'"):
+    with pytest.raises(ValueError, match="Unexpected attribute 'last_name' when instantiating class 'Student'"):
         Student(id=1, name='Luka', last_name='Mania', room=111)
+
+def test_model_init_raises_for_missing_attribute():
+    class Room(Model):
+        id: int
+        name: str
+
+    with pytest.raises(ValueError, match="Missing attribute 'name' when instantiating class 'Room'"):
+        Room(id=1)
