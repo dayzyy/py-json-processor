ede24a314d92c7e6530c4974a5619ca73eb5871d
Luka Mania <manialuka19@gmail.com>
2025-08-12 18:10:49 +0400

    feat: serialize students and rooms; combine them into a list

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from cli.parse_arguments import parse_arguments
 from file_handlers.base_file_readers import TextFileReader
 from file_handlers.base_file_writers import TextFileWriter
 from serializers.custom_serializers import StudentJSONSerializer, RoomJSONSerializer
+from app.combine import combine
 
 def main():
     args = parse_arguments()
 
     students_data = TextFileReader.read(args.student_file)
     rooms_data = TextFileReader.read(args.rooms_file)
+
+    students = StudentJSONSerializer.deserialize_list(students_data)
+    rooms = RoomJSONSerializer.deserialize_list(rooms_data)
+
+    combined = combine(students, rooms)
 
 if __name__ == "__main__":
     main()
