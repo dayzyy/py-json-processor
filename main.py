f11dbdd7e36a0f349f2fbb25ce5794a88b885be2
Luka Mania <manialuka19@gmail.com>
2025-08-12 18:00:30 +0400

    feat: parse arguments; read `student` and `rooms` json files

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from cli.parse_arguments import parse_arguments
+from file_handlers.base_file_readers import TextFileReader
+from file_handlers.base_file_writers import TextFileWriter
+from serializers.custom_serializers import StudentJSONSerializer, RoomJSONSerializer
 
 def main():
-    print("Hello from json-processor!")
+    args = parse_arguments()
 
+    students_data = TextFileReader.read(args.student_file)
+    rooms_data = TextFileReader.read(args.rooms_file)
 
 if __name__ == "__main__":
     main()
