335ad6ed9482861522a19428ab309d3787bfec8b
Luka Mania <manialuka19@gmail.com>
2025-08-12 21:54:51 +0400

    chore: add mapping of format/serializers to `settings.py`

--- settings.py
+++ settings.py
@@ -1,4 +1,11 @@
+from serializers.methods import json_serializer, xml_serializer
+
 # Serialization related constants
 JSON_INDENT = 2
 XML_ROOT_ELEMENT_NAME = 'rooms'
 XML_SHOW_ATTR_TYPE = False
+
+SERIALIZERS = {
+    "json": lambda data: json_serializer(data, indent=JSON_INDENT),
+    "xml": lambda data: xml_serializer(data, custom_root=XML_ROOT_ELEMENT_NAME, attr_type=XML_SHOW_ATTR_TYPE)
+}
