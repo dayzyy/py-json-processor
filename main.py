8a427bf039ff8e04098c93d4be68b7fd20b28d87
Luka Mania <manialuka19@gmail.com>
2025-08-12 17:00:40 +0400

    chore: move `combine` method to `app/combine.py`

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-from collections import defaultdict
-from typing import List
-from models.custom_models import Student, Room
-
-def combine(students: List[Student], rooms: List[Room]):
-    room_map = defaultdict(list)
-
-    for s in students:
-        room_map[s.room].append(vars(s))
-
-    result = []
-    for room in rooms:
-        result.append({
-            "id": room.id,
-            "name": room.name,
-            "students": room_map.get(room.id, [])
-        })
-
-    return result
-
 def main():
     print("Hello from json-processor!")
 
