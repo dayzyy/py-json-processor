b4015d8b5c79278036a1a08f88eab3287838fcca
Luka Mania <manialuka19@gmail.com>
2025-08-13 17:50:59 +0400

    refactor: rename (json/xml)serializers to exporters and move to exporters.py
    - Rename `json_serializer` -> `json_exporter` and `xml_serializer` -> `xml_exporter`
    - Move both functions into `exporters.py` file

--- exporters/exporters.py
+++ exporters/exporters.py
@@ -2,9 +2,9 @@ import json
 from typing import Any
 import dicttoxml
 
-def json_serializer(data: Any, indent: int = 2 ) -> str:
+def json_exporter(data: Any, indent: int = 2 ) -> str:
     return json.dumps(data, indent=indent)
 
-def xml_serializer(data: Any, *, custom_root: str = 'root', attr_type: bool = True) -> str:
+def xml_exporter(data: Any, *, custom_root: str = 'root', attr_type: bool = True) -> str:
     xml_bytes = dicttoxml.dicttoxml(data, custom_root=custom_root, attr_type=attr_type)
     return xml_bytes.decode() if isinstance(xml_bytes, bytes) else xml_bytes
